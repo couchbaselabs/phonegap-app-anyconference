7e9aa54f9cdcc5c4959b87d30864c562219f3cc2
Chris Anderson <jchris@couchbase.com>
2013-08-15 10:06:47 -0700

    remove overlay b/c uncanny valley

--- js/app/components/collectionView.js
+++ js/app/components/collectionView.js
@@ -22,28 +22,26 @@ define(function(require, exports, module) {
 
 	var CollectionView = Backbone.View.extend({
 	    manage: true,
-		
+
 		tagName: 'div',
 		className: 'anyconf-collection-container',
 		template: _.template(collectionTemplate),
-		
+
 		pages: [],
 		pagesByItemId: {},
-		
+
 		pageHeight: null,
 		prevPage: null,
 		currentPage: null,
 		nextPage: null,
 
-		pageOverlay: null,
-		
 		pointerStarted: false,
 		animating: false,
 		swipeChecked: false,
-		
+
 		routeId: null,
 		inView: false,
-		
+
 		events: {
 		    'pointerdown': 'pointerDown',
 		    'pointermove': 'pointerMove',
@@ -51,12 +49,12 @@ define(function(require, exports, module) {
 		    'pointercancel': 'pointerUp',
 		    'pointerleave': 'pointerUp'
 		},
-		
+
 		leave: function() {
 			// this.el.style.display = 'none';
 			this.transitionOut();
 		},
-		
+
         transitionFromClass: function(className) {
             var classList = this.el.classList;
 			if( classList.contains(className) ) {
@@ -68,7 +66,7 @@ define(function(require, exports, module) {
 			    classList.add(className);
 			}
         },
-		
+
 		setCurrentPage: function(itemPage) {
 			this.currentPage = itemPage;
 			this.nextPage = null;
@@ -96,27 +94,7 @@ define(function(require, exports, module) {
 				this.nextPage.render();
 			}
 		},
-		
-		positionOverlay: function() {
-            this.pageOverlay.classList.remove('js-page-overlay-removed');
-            this.listEl.insertBefore(this.pageOverlay, this.currentPage.el);
-		},
-		
-		updateOverlay: function() {
-			var _this = this;
-			_this.animating = true;
-			
-			var onTransitionEnd = function(evt) {
-				_this.animating = false;
-				console.log('overlay transition end');
-				evt.target.removeEventListener('webkitTransitionEnd', onTransitionEnd);
-				_this.positionOverlay();
-			};
 
-			this.pageOverlay.classList.add('js-page-overlay-removed');
-			this.pageOverlay.addEventListener('webkitTransitionEnd', onTransitionEnd);
-		},
-		
 		addToNewPage: function() {
 			this.currentPage = new ItemPageView({
 				pageHeight: this.pageHeight,
@@ -130,7 +108,7 @@ define(function(require, exports, module) {
 			this.currentPage.el.appendChild( itemView.el );
 			this.pagesByItemId[ itemView.model.id ] = this.currentPage;
 		},
-		
+
 		addItem: function(model) {
 			if( !this.currentPage ) {
 				this.addToNewPage();
@@ -139,7 +117,7 @@ define(function(require, exports, module) {
 				model: model,
 				parentView: this
 			}).render();
-			
+
 			// See if it fits on the page
 			this.addElToPage( itemView );
 			var viewBottom = itemView.el.offsetTop + itemView.el.offsetHeight;
@@ -154,46 +132,38 @@ define(function(require, exports, module) {
             this.currentPage.el.style.height = pageHeight + 'px';
             this.listEl.style.height = pageHeight + 'px';
 		},
-		
+
 		transitionToNext: function() {
 			var _this = this;
 
 			// animate current page up
 			this.currentPage.transitionOut(function() {
 				_this.setCurrentPage(_this.nextPage);
-				_this.updateOverlay();
-				_this.pageOverlay.style.opacity = null;
 			});
 		},
-		
+
 		transitionToPrevious: function() {
 			var _this = this;
 			// pull previous from top
 			this.prevPage.transitionIn(function() {
 				_this.setCurrentPage(_this.prevPage);
-				_this.positionOverlay();
 			});
 		},
-		
+
 		transitionPreviousAway: function() {
 			var _this = this;
 			this.prevPage.transitionOut(function() {
 				_this.setCurrentPage(_this.currentPage);
-				_this.positionOverlay();
 			});
 		},
-		
+
 		transitionCurrentBack: function() {
 			var _this = this;
-			this.pageOverlay.style.opacity = 1;
-
 			this.currentPage.transitionIn(function() {
 				_this.setCurrentPage(_this.currentPage);
-				_this.positionOverlay();
-				_this.pageOverlay.style.opacity = null;
 			});
 		},
-		
+
 		pointerDown: function(jqEvt) {
 		    var evt = jqEvt.originalEvent;
 		    var _this = this;
@@ -202,7 +172,7 @@ define(function(require, exports, module) {
 			    evt.preventDefault();
 				return;
 			}
-			
+
 			// evt.preventDefault();
 			// evt.stopPropagation();
 			this.startPoint = {
@@ -219,7 +189,6 @@ define(function(require, exports, module) {
 			}
 			this.targetEl = this.currentPage.el;
 			this.pageOffsetY = 0;
-			this.overlayOpacity = 0;
 
 			this.pointerStarted = true;
 			this.rAFIndex = requestAnimationFrame(function() {
@@ -228,7 +197,7 @@ define(function(require, exports, module) {
             this.currentPage.el.classList.remove('js-page-transition-in');
             this.currentPage.el.classList.remove('js-page-transition-out');
 		},
-		
+
 		pointerMove: function(jqEvt) {
 		    var evt = jqEvt.originalEvent;
 		    if( !this.pointerStarted ) {
@@ -237,7 +206,7 @@ define(function(require, exports, module) {
 		    }
 			console.log('pointermove');
 			evt.preventDefault();
-			
+
 			var targetEl = this.currentPage.el;
 			var prevEl;
 			if( this.prevPage ) {
@@ -252,12 +221,12 @@ define(function(require, exports, module) {
 				y: currentPoint.y - this.lastPoint.y
 			};
 			this.lastPoint = currentPoint;
-			
+
 			if( !this.swipeChecked ) {
                 // determine if scrolling or page swiping
                 var absX = Math.abs( this.lastDiff.x );
                 var absY = Math.abs( this.lastDiff.y );
-                
+
 				// More horizontal than vertical = swiping
 				this.swiping = (absX > absY);
 
@@ -273,7 +242,7 @@ define(function(require, exports, module) {
 				}
 				console.log('LIST: NOT SWIPING (allow scroll)');
             }
-			
+
 			var offsetY = currentPoint.y - this.startPoint.y;
 			if( offsetY < 0 ) {
 				// drag current page up
@@ -281,11 +250,7 @@ define(function(require, exports, module) {
 				this.pageOffsetY = offsetY;
 
 				// utils.setTransform(targetEl, 'translateY(' + offsetY + 'px) translateZ(0)');
-				var amount = -(offsetY / this.pageHeight);
-				this.overlayOpacity = (1 - amount).toFixed(2);
 
-				// this.pageOverlay.style.opacity = (1 - amount).toFixed(2);
-				
 				this.pendingPage = this.nextPage;
 			} else if( prevEl ) {
 				// drag previous page down
@@ -306,7 +271,7 @@ define(function(require, exports, module) {
             }
             */
 		},
-		
+
 		handleEmptyPointerUp: function(jqEvt) {
 		    jqEvt.preventDefault();
 		    if( jqEvt.target.tagName === 'A' ) {
@@ -314,23 +279,23 @@ define(function(require, exports, module) {
 		        appRouter.navigate(href, {trigger: true});
 		    }
 		},
-		
+
 		pointerUp: function(jqEvt) {
 		    console.log('pointerup');
 		    var evt = jqEvt.originalEvent;
 		    cancelAnimationFrame(this.rAFIndex);
-		    
+
 		    if( !this.currentPage ) {
 		        this.handleEmptyPointerUp(jqEvt);
 		        return;
 		    }
-		    
+
 			var targetEl = this.currentPage.el;
-			
+
 		    if( !this.pointerStarted ) {
 		        return;
 		    }
-			
+
 			if( this.swiping ) {
 			    this.swiping = false;
 			}
@@ -341,7 +306,7 @@ define(function(require, exports, module) {
             }
             evt.preventDefault(); // this prevents click event
             evt.stopPropagation();
-            
+
 			if( this.lastDiff.y > 0 ) {
 				if( !this.prevPage && this.pendingPage ) {
 					this.transitionCurrentBack();
@@ -371,8 +336,7 @@ define(function(require, exports, module) {
 			}
 
 			console.log('DRAW', this.pageOffsetY);
-			utils.setTransform(this.targetEl, 'translateY(' + this.pageOffsetY + 'px) translateZ(0)');	
-			this.pageOverlay.style.opacity = this.overlayOpacity;
+			utils.setTransform(this.targetEl, 'translateY(' + this.pageOffsetY + 'px) translateZ(0)');
 		},
 
 		beforeRender: function() {
@@ -382,48 +346,46 @@ define(function(require, exports, module) {
             this.pages = [];
             this.pagesByItemId = {};
 		},
-		
+
         afterRender: function() {
             this.inView = true;
             this.el.style.display = 'block';
             this.listEl = this.$el.find('.js-item-view-container')[0];
             // var viewType = this.options.type;
-            
+
             if( this.options.filter ) {
                 var filteredModels = this.collection.filter(this.options.filter);
                 this.subCollection = new Backbone.Collection(filteredModels);
             } else {
                 this.subCollection = this.collection;
             }
-            
+
             this.subCollection.each(function(itemModel) {
                 this.addItem(itemModel);
             }, this);
-		    
+
 		    if( this.subCollection.length == 0 ) {
 		        this.showEmptyPage();
 		        this.transitionIn();
 		        return;
 		    }
-			
+
 			var itemId = appRouter.getSubRoute();
 		    var page = this.pagesByItemId[ itemId ] || this.pages[0];
 
             // appView.setCurrentView(this);
 			this.setCurrentPage( page );
-			
-			this.positionOverlay();
-			
+
 			this.transitionIn();
 			if( this.checkTime ) {
 			    this.checkTime.call(this);
 			}
         },
-        
+
         destroy: function() {
             this.$el.remove();
             this.inView = false;
-        }, 
+        },
 
         transitionIn: function() {
 		    var _this = this;
@@ -437,7 +399,7 @@ define(function(require, exports, module) {
 		        _this.transitionFromClass('js-enter-view-transition');
 		        utils.setTransform(el, null); // 'none';
 		    }, 1);
-		    
+
 			var onTransitionEnd = function(evt) {
 				_this.animating = false;
                 // el.style.overflow = null;
@@ -446,7 +408,7 @@ define(function(require, exports, module) {
 			};
 			el.addEventListener('webkitTransitionEnd', onTransitionEnd);
         },
-		        
+
         transitionOut: function() {
 		    var _this = this;
 		    var el = this.el;
@@ -457,7 +419,7 @@ define(function(require, exports, module) {
 		        _this.transitionFromClass('js-leave-view-transition');
     		    utils.setTransform(el, 'translateX(-' + window.innerWidth + 'px) translateZ(0)');
 		    }, 1);
-		    
+
 			var onTransitionEnd = function(evt) {
 				_this.animating = false;
                 el.style.overflow = null;
@@ -468,16 +430,16 @@ define(function(require, exports, module) {
 			};
 			el.addEventListener('webkitTransitionEnd', onTransitionEnd);
         },
-		
+
 		serialize: function() {
 		},
-		
+
 		hide: function() {
 			// this.el.removeEventListener('pointerdown', this.pointerDown);
 			this.pointerStarted = false;
 			this.el.style.display = 'none';
 		},
-		
+
 		initialize: function() {
 			var _this = this;
 
@@ -495,11 +457,9 @@ define(function(require, exports, module) {
 			    pointerleave: documentPointerUp,
 			    pointerup: documentPointerUp
 			});
-		    
+
 			this.pageHeight = window.innerHeight;
-			this.pageOverlay = document.createElement('div');
-			this.pageOverlay.className = 'js-page-overlay';
-			
+
 		    appRouter.on('route', function(route, itemId) {
 		        if( route == this.routeId ) {
 		            if( itemId && itemId.length == 1) {
